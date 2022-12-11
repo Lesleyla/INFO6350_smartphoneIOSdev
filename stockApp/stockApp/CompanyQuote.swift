db7aad3d43482107ca85754fd9c0b9ea5bbc4fd2
Lesleyla <lesleyguo98@outlook.com>
2022-12-10 17:52:57 -0800

    final

---
+++ stockApp/stockApp/CompanyQuote.swift
@@ -0,0 +1,14 @@
+//
+//  CompanyQuote.swift
+//  stockApp
+//
+//  Created by Mengyang Guo on 12/10/2022.
+//
+
+import Foundation
+
+class CompanyQuote {
+    var symbol: String = ""
+    var name: String = ""
+    var price: Float = 0.0
+}
